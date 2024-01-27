28299c7027f9d8dbd4a3dd362fb490691fd65b6f
xakepa <plamen_ivanov@mail.com>
2024-01-27 17:55:41 +0200

    Localize to BG

--- components/Scheduler.jsx
+++ components/Scheduler.jsx
@@ -5,6 +5,18 @@ import 'dayjs/locale/bg'
 
 dayjs.locale('bg')
 const localizer = dayjsLocalizer(dayjs)
+const messages = {
+    today: 'Днес',
+    previous: 'Предишен',
+    next: 'Следващ',
+    month: 'Месец',
+    week: 'Седмица',
+    day: 'Ден',
+    agenda: 'Събития',
+    date: 'Дата',
+    time: 'Време',
+    event: 'Събитие'
+}
 const events = [
     {
         start: dayjs('2024-01-28T12:00:00').toDate(),
@@ -27,6 +39,7 @@ const Scheduler = () => {
                 endAccessor="end"
                 popup={true}
                 events={events}
+                messages={messages}
                 style={{
                     height: "95vh",
                     width: "90%"
